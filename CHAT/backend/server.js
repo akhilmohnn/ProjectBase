f059fa79afae9ab7b804d7a319f932b8ab77854c
akhilmohnn <akhilmohnn@outlook.com>
2024-10-23 21:11:14 +0530

    Basic+ added auto save package

---
+++ CHAT/backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app=express();
+
+app.get("/", (req,res)=> {
+    //root route http://localhost.5000/ 
+    res.send("Hello world!!!");
+    });
+    
+app.listen(5000,()=> console.log('Server is running on 5000'));
+
